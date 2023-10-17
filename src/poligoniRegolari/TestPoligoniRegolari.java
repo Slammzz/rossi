791b060adb7d0d69d23fabfafc59e81eee65f7e4
samuel.rossi <samuel.rossi@buonarroti.tn.it>
2023-10-17 09:43:38 +0200

    feat: Instanzazione classi in class TestPoligoniRegolari

--- src/poligoniRegolari/TestPoligoniRegolari.java
+++ src/poligoniRegolari/TestPoligoniRegolari.java
@@ -1,4 +1,9 @@
 package poligoniRegolari;
 
 public class TestPoligoniRegolari {
+    GestionePoligoni g1 = new GestionePoligoni(3);
+    Quadrato q1 = new Quadrato(3);
+    Rombo r1 = new Rombo(5, 11,9);
+    Pentagono p1 = new Pentagono(2);
+    Esagono e1 = new Esagono(23);
 }
