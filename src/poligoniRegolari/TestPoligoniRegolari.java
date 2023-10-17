5fe0c7dc714c59066b00fcfeb422457602307e4e
samuel.rossi <samuel.rossi@buonarroti.tn.it>
2023-10-17 09:58:37 +0200

    feat: Prova inserimento poligono in class TestPoligoniRegolari

--- src/poligoniRegolari/TestPoligoniRegolari.java
+++ src/poligoniRegolari/TestPoligoniRegolari.java
@@ -1,9 +1,15 @@
 package poligoniRegolari;
 
 public class TestPoligoniRegolari {
-    GestionePoligoni g1 = new GestionePoligoni(3);
-    Quadrato q1 = new Quadrato(3);
-    Rombo r1 = new Rombo(5, 11,9);
-    Pentagono p1 = new Pentagono(2);
-    Esagono e1 = new Esagono(23);
+    public static void main(String[] args) {
+        GestionePoligoni g1 = new GestionePoligoni(3);
+
+        Quadrato q1 = new Quadrato(3);
+        Rombo r1 = new Rombo(5, 11,9);
+        Pentagono p1 = new Pentagono(2);
+        Esagono e1 = new Esagono(23);
+        g1.inserisciPoligono(q1);
+        System.out.println(g1.toString());
+    }
+
 }
