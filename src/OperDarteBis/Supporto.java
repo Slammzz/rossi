3c78b2e847b2e3ffc335ecf36e304dcdf8e594d4
samuel.rossi <samuel.rossi@buonarroti.tn.it>
2023-11-14 10:12:38 +0100

    FEAT: creazione metodo costruttore

--- src/OperDarteBis/Supporto.java
+++ src/OperDarteBis/Supporto.java
@@ -5,4 +5,13 @@ public class Supporto {
     private double profondita;
     private double lunghezza;
     private double larghezza;
+
+    public Supporto(String materiale, double lunghezza, double larghezza, double profondita){
+        this.larghezza=larghezza;
+        this.lunghezza=lunghezza;
+        this.profondita=profondita;
+        this.materiale=materiale;
+    }
+
+
 }
